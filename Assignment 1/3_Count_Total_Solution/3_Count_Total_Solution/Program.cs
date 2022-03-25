15229986694ec419e1dd4bd489ddcad96dbabd48
kedardhumal7028 <kedardhumal31@gmail.com>
2022-03-25 15:18:34 +0530

    Assignment Programs

---
+++ Assignment 1/3_Count_Total_Solution/3_Count_Total_Solution/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _3_Count_Total_Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N,Cube,Cnt=0;
+
+            Console.Write("\n Enter Number : ");
+            N = Convert.ToInt32(Console.ReadLine());
+
+            for (; N <= 0 || N == null; )
+            {
+                Console.Write("\n Enter Number : ");
+                N = Convert.ToInt32(Console.ReadLine());
+            }
+
+                for (int i = 1; i <= N / 2; i++)
+                {
+                    for (int j = 1; j <= N / 2; j++)
+                    {
+                        Cube = (i * i * i) + (j * j * j);
+
+                        if (Cube == N)
+                        {
+                            Cnt++;
+                        }
+                    }
+                }
+
+            Console.WriteLine("\n There are " + Cnt + "  Cube Addition is Equal To " + N);
+
+            Console.ReadKey();
+        }
+    }
+}
